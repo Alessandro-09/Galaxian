996ad54b438006a807e8f477d613cd051e7d522c
watson234 <165717299+watson234@users.noreply.github.com>
2025-06-17 23:38:00 -0600

    Add files via upload

--- include/Game.hpp
+++ include/Game.hpp
@@ -3,6 +3,19 @@
 
 #include "Init.hpp"
 #include <vector>
+#include <iostream>
+#include <stdio.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <algorithm>
+#include <random>
+#include <string>
 
 struct Star {
     float x;
@@ -17,12 +30,22 @@ public:
     Game(ALLEGRO_FONT* font, int width, int height);
     int run(SystemResources& sys);
     void draw() const;
+    void dibujarenemigos() const;
+    void dibujarnave() const;
+    void dibujarbala() const;
 
 private:
     ALLEGRO_FONT* font;
     int width, height;
-    std::vector<Star> stars;
 
+    std::vector<Star> stars;
+    void crearnivel();
+    void crearbala();
+    void colisiones();
+    void actualizarenemigos();
+    void actualizarbala();
+    void actualizarNave();
+    void crearnave();
     void generateStars();
     void update();
     float starSpeed;
